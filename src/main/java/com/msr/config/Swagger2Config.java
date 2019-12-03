8b1a676251b5783bfe018238dcdf662b42d6129d
Kyle Gowen <kyle@measurabl.com>
2019-12-03 21:37:48 +0000

    Backend Hiring take home.  UTs and sample route.

--- src/main/java/com/msr/config/Swagger2Config.java
+++ src/main/java/com/msr/config/Swagger2Config.java
@@ -14,11 +14,11 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class Swagger2Config {
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors
-                        .basePackage("com.msr"))
+                .apis(RequestHandlerSelectors.basePackage("com.msr"))
                 .paths(PathSelectors.any())
                 .build().apiInfo(apiEndPointsInfo());
     }
